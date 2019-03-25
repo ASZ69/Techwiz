4791a6476cc1d8a05af8ac6d9579937bec6679b0
889149885 <a889.149885@gmail.com>
2019-03-25 20:09:34 +0500

    code

--- js/jobs-view-controller.js
+++ js/jobs-view-controller.js
@@ -13,15 +13,15 @@ jobs.controller("jobViewController", function ($scope, $filter) {
 	
 	var url = window.location.search.replace("?", "").replaceAll("%22", "").replaceAll("%20", " ");
     if (url == "") {
-        $('.heading').html('You cannot access this page directly...');
+        $('body').append('<h1>You cannot access this page directly...</h1>');
     } else {
         var parts = url.split("&");
         var mainCat = parts[0].split("=")[1];
-        var subCat = parts[1].split("=")[1];
+        $scope.subCat = parts[1].split("=")[1];
         var company = parts[2].split("=")[1];
 
         $scope.subCats = $filter('filter')(dataList, { mainCat: mainCat })[0].subCats;
-        $scope.companiesData = $filter('filter')($scope.subCats, { name: subCat })[0].data;
+        $scope.companiesData = $filter('filter')($scope.subCats, { name: $scope.subCat })[0].data;
         $scope.jobData = $filter('filter')($scope.companiesData, { companyTitle: company })[0];
     }
 
