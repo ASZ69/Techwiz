5c7192961d3028f46bd363ed0b9e97539b3704f3
889149885 <a889.149885@gmail.com>
2019-03-27 18:08:07 +0500

    dsfsdfsd

--- js/common.js
+++ js/common.js
@@ -6,21 +6,28 @@ $(function () {
 		var $nav = $(".navbar-fixed-top");
 		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
-    
 });
 
 $(document).ready(function(){
+	var views = window.localStorage.getItem("pgView");
+	$('#pgView').html(parseInt(views)+1);
+	window.localStorage.setItem("pgView",parseInt(views)+1);
+	
+	$('#date').html(new Date().getFullYear() +"-"+(parseInt(new Date().getMonth())+1)+"-"+new Date().getDate());
+	
    var auth = window.localStorage.getItem("auth");
+   var name = window.localStorage.getItem("name");
     if(auth == "true"){
-        $('#navRegister').html('<li class="nav-item sign ml-auto"><a href="view/auth.html" class="nav-link">Sign out</a></li>');
+        $('#navRegister').html('<li class="nav-item sign ml-auto"><a href="#" class="nav-link">Welcome '+name+'</a></li><li onClick="signOut()" class="nav-item sign ml-auto"><a href="" class="nav-link">Sign out</a></li>');
     }
     else{
 		$('#startPanel').css("display","block");
-//        $('#navRegister').html(' <li class="nav-item sign ml-auto"><a href="view/auth.html" class="nav-link">Sign in</a></li><li class="nav-item sign ml-auto"> <a href="view/auth.html" class="nav-link">Sign up</a></li>');
     }
     
 });
 
-function openPanel(){
-	$('#startPanel').html("display","block");
+function signOut(){
+	window.localStorage.removeItem("auth");
+	window.localStorage.removeItem("name");
+	window.localStorage.removeItem("type");
 }
